f07cfe61160a6a6206c0b7116a1bee86f719b38e
Gurunath <bhuvi23389@gmail.com>
2022-04-20 17:43:26 +0100

    Active navbar link to be highlighted

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,8 +5,23 @@ export const Nav: React.FC = () =>
     <nav className="nav__container">
         <section className="nav__logo">FAKELANDIA JUSTICE DEPARTMENT</section>
         <ul className="nav__container nav__ul">  
-            <li><NavLink to = '/'>Home</NavLink> </li>
-            <li><NavLink to = '/misdemeanours'>Misdemeanours</NavLink></li>
-            <li><NavLink to = '/confession'>Confess To us</NavLink></li>
+            <li>
+                <NavLink to ='/' 
+                className = {({ isActive }) => (isActive ? "nav__link--active" : "nav__link")}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to ='/misdemeanours' 
+                className = {({ isActive }) => (isActive ? "nav__link--active" : "nav__link")}>
+                    Misdemeanours
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to ='/confession' 
+                className = {({ isActive }) => (isActive ? "nav__link--active" : "nav__link")}>
+                    Confess To us
+                </NavLink>
+            </li>
         </ul>
     </nav>

85df87955b4235713b2cbc43d5990bb10092c3c9
Gurunath <bhuvi23389@gmail.com>
2022-04-21 14:06:00 +0100

    Added filter option to the misdemeanour column

---
+++ src/components/misdemeanourFilter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface MisdemeanourFilterProps {
+    columnName: string;
+    onChangeFilter: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export const MisdemeanourFilter: React.FC<MisdemeanourFilterProps> = ({columnName, onChangeFilter}) => 
+    <div className="misdemean__item item--title title--misdemean">
+        
+            <div>{columnName}</div> 
+            <select onChange={onChangeFilter}>
+                <option value='all'>All</option>
+                <option value='rudeness'>Rudeness</option>
+                <option value='vegetables'>Vegetables</option>
+                <option value='lift'>Lift</option>
+                <option value='united'>United</option>
+            </select> 
+        
+    </div>
--- src/components/misdemeanours.tsx
+++ src/components/misdemeanours.tsx
@@ -1,24 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { MISDEMEANOURS } from "../docs/generate_misdemeanours";
 import { IMisdemeanour, MisdemeanourContext } from "./misdemeanourContext";
+import { MisdemeanourFilter } from "./misdemeanourFilter";
 import { MisdemeanourTable } from "./misdemeanourTable";
 
 export const Misdemeanours: React.FC = () => {
+    const [filtervalue, setFilterValue] = useState("all");
+    const [localMisdemeanours, setLocalMisdemeanours] = useState<Array<IMisdemeanour>>([]);
 
     const misdemeanours: IMisdemeanour[] = useContext(MisdemeanourContext);
+
+    useEffect(()=> {
+        if (filtervalue !== 'all') 
+            setLocalMisdemeanours(
+                misdemeanours.filter(misdemeanour => 
+                        misdemeanour.misdemeanour === filtervalue));
+        else
+            setLocalMisdemeanours(misdemeanours)
+    },[misdemeanours, filtervalue]);
 
     return (
         <div className="misdemean__container">
             <div className="misdemean__item item--title misdemean__item--citizenId">Citizen ID</div>
             <div className="misdemean__item item--title">Date</div>
-            <div className="misdemean__item item--title">Misdemeanour</div>
+            <MisdemeanourFilter columnName="Misdemeanour" onChangeFilter={e => setFilterValue(e.target.value)} />
             <div className="misdemean__item item--title">Punishment Idea</div>
-            {misdemeanours.map ((misdemeanour, index) => 
+    
+            {localMisdemeanours.map ((misdemeanour, index) => 
                     <MisdemeanourTable 
+                        key = {index}
                         citizenId={misdemeanour.citizenId}
                         date={misdemeanour.date}
                         misdemeanour= {misdemeanour.misdemeanour}/>
             )}
-            
         </div>
     );
 }

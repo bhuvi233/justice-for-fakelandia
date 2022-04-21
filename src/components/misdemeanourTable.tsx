6cdd1c7f72e74c793441a0c21c49ff50fab459c6
Gurunath <bhuvi23389@gmail.com>
2022-04-21 11:16:25 +0100

    Added emoji for the Misdemeanour

--- src/components/misdemeanourTable.tsx
+++ src/components/misdemeanourTable.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Misdemeanour, MISDEMEANOURS } from "../docs/generate_misdemeanours";
 import { IMisdemeanour } from "./misdemeanourContext";
+
+const misdemeanourEmojiMap = new Map <Misdemeanour, string>();
+misdemeanourEmojiMap.set(MISDEMEANOURS[0], '🤪' );
+misdemeanourEmojiMap.set(MISDEMEANOURS[1], '🥗' );
+misdemeanourEmojiMap.set(MISDEMEANOURS[2], '🗣') ;
+misdemeanourEmojiMap.set(MISDEMEANOURS[3], '😈');
+
 
 export const MisdemeanourTable : React.FC<IMisdemeanour> = ({citizenId, date, misdemeanour}) =>
     <>
         <div className="misdemean__item misdemean__item--citizenId">{citizenId}</div>
         <div className="misdemean__item">{date}</div>
-        <div className="misdemean__item">{misdemeanour}</div>
+        <div className="misdemean__item">{misdemeanour} = {misdemeanourEmojiMap.get(misdemeanour)}</div>
         <div className="misdemean__item"><img src='https://picsum.photos/200/200' alt = "alternative"/></div>
     </>
--- src/components/misdemeanours.tsx
+++ src/components/misdemeanours.tsx
@@ -8,10 +8,10 @@ export const Misdemeanours: React.FC = () => {
 
     return (
         <div className="misdemean__container">
-            <div className="misdemean__item misdemean__item--citizenId">Citizen ID</div>
-            <div className="misdemean__item">Date</div>
-            <div className="misdemean__item">Misdemeanour</div>
-            <div className="misdemean__item">Punishment Idea</div>
+            <div className="misdemean__item item--title misdemean__item--citizenId">Citizen ID</div>
+            <div className="misdemean__item item--title">Date</div>
+            <div className="misdemean__item item--title">Misdemeanour</div>
+            <div className="misdemean__item item--title">Punishment Idea</div>
             {misdemeanours.map ((misdemeanour, index) => 
                     <MisdemeanourTable 
                         citizenId={misdemeanour.citizenId}
